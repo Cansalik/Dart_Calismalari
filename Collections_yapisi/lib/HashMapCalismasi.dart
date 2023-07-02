c20079e3b0830629ced401dc473d65be5ef42306
Neset Can Salik <neset.salik@gmail.com>
2023-07-02 15:25:19 +0300

    HashMap yapısı tamamlanıp, nesne tabanlı programlama kullanılarak örnek yapıldı.

--- Collections_yapisi/lib/HashMapCalismasi.dart
+++ Collections_yapisi/lib/HashMapCalismasi.dart
@@ -1,23 +1,39 @@
 import 'dart:collection';
 
-void main()
-{
-  var iller = HashMap<int, String>();
+    void main()
+    {
+      var iller = HashMap<int, String>();
 
-  iller[01] = "Adana";
-  iller[02] = "Adıyaman";
-  iller[06] = "Anakra";
-  iller[21] = "Diyarbakır";
-  iller[34] = "İstanbul";
-  iller[35] = "İzmir";
+      iller[01] = "Adana";
+      iller[02] = "Adıyaman";
+      iller[06] = "Anakra";
+      iller[21] = "Diyarbakır";
+      iller[34] = "İstanbul";
+      iller[35] = "İzmir";
 
-  print(iller);
+      print(iller);
 
-  iller[01] = "Yeni Adana";
+      iller[01] = "Yeni Adana";
 
-  print(iller);
+      print(iller);
 
-  String veri = iller[21]!;
-  print(veri);
+      String veri = iller[21]!;
+      print(veri);
 
-}
+      print(iller.length);
+
+      print(iller.isEmpty);
+      print(iller.containsKey(17));
+      print(iller.containsValue("Diyarbakır"));
+
+      var anahtarlar = iller.keys;
+      for(var a in anahtarlar)
+      {
+        print("Sonuç: ${iller[a]}");
+      }
+
+      iller.remove(02);
+      print(iller);
+      iller.clear();
+      print(iller);
+    }
---
+++ Collections_yapisi/lib/HashMapNesneTabanli.dart
@@ -0,0 +1,32 @@
+import 'dart:collection';
+
+import 'package:collections_yapisi/Kisiler.dart';
+
+void main()
+{
+  var K1 = Kisiler(1234, "Ahmet");
+  var K2 = Kisiler(1646, "Mehmet");
+  var K3 = Kisiler(1126, "Zeynep");
+
+  var kisiler = HashMap<int,Kisiler>();
+
+  kisiler[K1.tcno] = K1;
+  kisiler[K2.tcno] = K2;
+  kisiler[K3.tcno] = K3;
+
+  var anahtarlar = kisiler.keys;
+
+  for(var a in anahtarlar)
+  {
+    var kisi = kisiler[a];
+
+    if(kisi != null)
+    {
+      print("*** *** ***");
+      print("Kişi No: ${kisi.tcno}");
+      print("Kişi Ad: ${kisi.ad}");
+    }
+  }
+
+
+}
